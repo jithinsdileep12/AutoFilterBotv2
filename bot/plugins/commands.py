64b0fb9291dee0323ae666a6d8da9b3fe88fb1a1
jithinsdileep12 <89510784+jithinsdileep12@users.noreply.github.com>
2021-12-05 18:45:18 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file name} /n @malrok",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    ' our group ', url="https://t.me/kjmods0707"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/kjmods0707'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/jithinsdileep12/AutoFilterBotv2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/kjmods0707')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
