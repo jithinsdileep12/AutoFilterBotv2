52d65ad3dc9de0a06710b949d14473f58f60bcef
jithinsdileep12 <89510784+jithinsdileep12@users.noreply.github.com>
2021-12-06 13:08:16 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -48,7 +48,7 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="http://t.me/kjmods">jithin s dileep</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="http://t.me/kjmods">bot_kj_bot</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
